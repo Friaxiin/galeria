e3c73d397f613255d54607be97710c7d39bce6e2
patryk.labuz <patryk.labuz@S50-11>
2024-12-13 09:54:18 +0100

    Dodaj pliki projektów.

---
+++ galeria/Images.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace galeria
+{
+    public class Images
+    {
+        public string src;
+        public string name;
+        public string author;
+        public DateTime date;
+
+    }
+}
---
+++ galeria/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace galeria
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<Image> images = new List<Image>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void GetImages()
+        {
+            string[] txt = File.ReadAllLines("data.txt");
+
+            foreach (string s in txt)
+            {
+                Images img = new Images();
+                
+                string[] image = s.Split(",");
+
+                img.src = image[0];
+                img.name = image[1];
+                img.author = image[2];
+                img.date = DateTime.Parse(image[3]);
+
+                StackPanel stackPanel = new StackPanel();
+                Image imgLbl = new Image();
+                imgLbl.Source = new BitmapImage(new Uri("", UriKind.Relative)); ;
+                //panel.Children.Add();
+            }
+            
+        }
+    }
+}
